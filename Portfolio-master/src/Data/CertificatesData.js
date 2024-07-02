6a1fddf1cf1da50c40064c43572c4861cd27659b
Jerome Oswald Ebenezer J <jeromeoswald7@gmail.com>
2024-07-02 20:20:59 +0530

    portfolio

---
+++ Portfolio-master/src/Data/CertificatesData.js
@@ -0,0 +1,29 @@
+let certificationsList=[
+    {
+        title:"Web Development BootCamp",
+        instructor:"Angela Yu",
+        link:"https://drive.google.com/file/d/1w4FPyEozyFhUO8CQgSoaG8-IlKqpzea8/view?usp=sharing",
+        platform:'udemy'
+    },
+    {
+        title:"React Js",
+        instructor:"Maximilian Schwarzmuller",
+        link:"#",
+        platform:'Guvi'
+    },
+    {
+        title:"Node Js",
+        instructor:"Maximilian Schwarzmuller",
+        link:"#",
+        platform:'Cisco'
+    },
+    {
+        title:"Flutter Development",
+        instructor:"Maximilian Schwarzmuller",
+        link:"https://drive.google.com/file/d/1iGTIn8QLcY1JCqWE-CCIWE5QP5c2cHZp/view?usp=sharing",
+        platform:'Guvi'
+    },
+];
+
+const CertificatesData={certificationsList};
+export default CertificatesData;
---
+++ Portfolio-master/src/Data/EducationData.js
@@ -0,0 +1,51 @@
+const languagesDone = [
+    {
+        name: '.NET',
+        percentDone: 90
+    },
+    {
+        name: 'React Js',
+        percentDone: 80
+    },
+    {
+        name: 'Node Js',
+        percentDone: 80
+    },
+    {
+        name: 'Python',
+        percentDone: 70
+    },
+    {
+        name: 'MySQL',
+        percentDone: 90
+    },
+    {
+        name: 'Mongodb',
+        percentDone: 80
+    },
+    {
+        name: 'UX Design - Figma',
+        percentDone: 80
+    }
+]
+const EducationData={
+    collegeName:"Bharathidasan University",
+    courseName:"Master of Computer Application",
+    couseStartYear:"2017",
+    courseEndYear:"2019",
+    details:[
+        'Relevant Courses: Major in Computer Application.',
+        'I have done courses on Data Science, Cloud Computing and Full Stack Development.'
+    ],
+    codingStatus:languagesDone,
+    collegeName1:"Bishop Heber College",
+    courseName1:"B.Sc. Computer Science",
+    couseStartYear1:"2014",
+    courseEndYear1:"2017",
+    details1:[
+        'Relevant Courses: Major in Computer Science.',
+        'I have done courses on Microsoft Resourses, UX Design.'
+    ],
+    codingStatus1:languagesDone
+}
+export default EducationData;
