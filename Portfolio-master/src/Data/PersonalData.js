a776b09eaf77917006a9741cbc78e15541ffa1d7
Jerome Oswald Ebenezer J <jeromeoswald7@gmail.com>
2024-07-09 11:03:13 +0530

    resume portfolio

--- Portfolio-master/src/Data/PersonalData.js
+++ Portfolio-master/src/Data/PersonalData.js
@@ -6,7 +6,7 @@ const PersonalData = {
     email:"jeromeoswald7@gmail.com",
     address:"India",
     aboutMe:"Hi Everyone, I am Jerome Oswald Ebenezer J from Tamil Nadu, India.I learned .Net, Python, JavaScript etc, Worked with libraries & frameworks like ReactJS, NodeJS. Have a keen knowledge of databases like MySQL, MongoDB. Eager to contribute to innovative projects and gain hands-on experience in a dynamic and collaborative",
-    resumeLink:'https://drive.google.com/file/d/13wx5dNChAVWGBVhMHauP1SAUh2C55eRv/view?usp=sharing',
+    resumeLink:'https://drive.google.com/file/d/1zVplp_7-nB0_gdziByIncEw-ksquodOS/view?usp=drive_link',
     imageUrl:''
 }
 export const autoTypeData=['Software Developer', 'MERN Full Stack Developer','Frontend Developer','Backend Developer','.Net Developer', 'Open Source Contributor'];
